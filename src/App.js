9d440e092bd9cc467c67a46431459bb25dce6cf7
pedram.enayati.kashani <pedram.en1376@gmail.com>
2024-07-27 20:24:21 +0330

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = ()=>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>اپلیکیشم مدیریت مخاطبین</h3>
+        <button className="btn btn-primary">Button <i className="fa fa-user"></i></button>
     </div>
   );
 }
