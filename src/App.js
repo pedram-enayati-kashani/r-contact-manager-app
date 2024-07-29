558cd17a23cffce5f6475543d9a62682cd1a28a8
pedram.enayati.kashani <pedram.en1376@gmail.com>
2024-07-29 12:12:56 +0330

    add navbar and colors.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from "./components/Navbar";
 
 const App = ()=>{
   return (
     <div className="App">
-      <h3>اپلیکیشم مدیریت مخاطبین</h3>
-        <button className="btn btn-primary">Button <i className="fa fa-user"></i></button>
+      <Navbar/>
     </div>
   );
 }
