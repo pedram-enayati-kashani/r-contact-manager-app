1135230e88f3a51dcec92ef8a1e40c2350b20d39
pedram.enayati.kashani <pedram.en1376@gmail.com>
2024-08-02 12:48:47 +0330

    add card

---
+++ src/components/contact/Contacts.jsx
@@ -0,0 +1,14 @@
+import Contact from "./Contact";
+
+const Contacts = () => {
+    return (
+        <section className="container">
+            <div className="row">
+                {/* contact */}
+                <Contact/>
+            </div>
+        </section>
+    );
+};
+
+export default Contacts;
