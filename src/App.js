1b6cef7e3297b49eea46f1262338a41599d9ff83
pedram.enayati.kashani <pedram.en1376@gmail.com>
2024-09-08 21:12:47 +0330

    create add contact page

--- src/App.js
+++ src/App.js
@@ -1,50 +1,105 @@
-import {useState,useEffect} from "react";
-import './App.css';
-import {Routes,Route,Navigate} from "react-router-dom";
-import axios from "axios";
+import { useState, useEffect } from "react";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+
 import {
-    AddContact,
-    EditContact,
-    Contacts,
-    Contact,
-    Navbar
+  AddContact,
+  Contact,
+  Contacts,
+  EditContact,
+  Navbar,
 } from "./components";
 
-import {getAllContacts ,getAllGroups} from './services/contactService'
+import {
+  getAllContacts,
+  getAllGroups,
+  createContact,
+} from "./services/contactService";
 
-const App = ()=>{
-    const [getContacts, setContacts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [getGroups, setGroups] = useState([]);
-    useEffect(()=>{
-        const fetchData = async () => {
-            try{
-                setLoading(true);
-                const  {data:contactsData} = await getAllContacts();
-                const {data:groupsData} = await getAllGroups();
-                setContacts(contactsData);
-                setGroups(groupsData);
-                setLoading(false);
-            }catch (err) {
-                console.log(err.message)
-                setLoading(false)
-            }
-        }
+import "./App.css";
 
-        fetchData();
-    },[]);
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [getContacts, setContacts] = useState([]);
+  const [getGroups, setGroups] = useState([]);
+  const [getContact, setContact] = useState({
+    fullname: "",
+    photo: "",
+    mobile: "",
+    email: "",
+    job: "",
+    group: "",
+  });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const { data: contactsData } = await getAllContacts();
+        const { data: groupsData } = await getAllGroups();
+
+        setContacts(contactsData);
+        setGroups(groupsData);
+
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const createContactForm = async (event) => {
+    event.preventDefault();
+    try {
+      const { status } = await createContact(getContact);
+
+      if (status === 201) {
+        setContact({});
+        navigate("/contacts");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const setContactInfo = (event) => {
+    setContact({
+      ...getContact,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div className="App">
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Navigate to="/contacts"/>}/>
-            <Route path="/contacts" element={<Contacts contacts={getContacts} loading={loading}/>}/>
-            <Route path="/contacts/add" element={<AddContact/>}/>
-            <Route path="/contacts/:contactId" element={<Contact/>}/>
-            <Route path="/contacts/edit/:contactId" element={<EditContact/>}/>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts contacts={getContacts} loading={loading} />}
+        />
+        <Route
+          path="/contacts/add"
+          element={
+            <AddContact
+              loading={loading}
+              setContactInfo={setContactInfo}
+              contact={getContact}
+              groups={getGroups}
+              createContactForm={createContactForm}
+            />
+          }
+        />
+        <Route path="/contacts/:contactId" element={<Contact />} />
+        <Route path="/contacts/edit/:contactId" element={<EditContact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,6 @@
 import Contact from "./Contact";
 import Spinner from '../Spinner';
+import {Link} from "react-router-dom";
 import {CURRENTLINE,ORANGE,PINK} from "../../helpers/colors";
 
 const Contacts = ({contacts,loading}) => {
@@ -10,10 +11,10 @@ const Contacts = ({contacts,loading}) => {
                 <div className="row">
                     <div className="col">
                         <p className="h3">
-                            <button className="btn mx-2" style={{backgroundColor: PINK}}>
+                            <Link to={"/contacts/add"} className="btn mx-2" style={{backgroundColor: PINK}}>
                                 ساخت مخاطب جدید
                                 <i className="fa fa-plus-circle mx-2"/>
-                            </button>
+                            </Link>
                         </p>
                     </div>
                 </div>
